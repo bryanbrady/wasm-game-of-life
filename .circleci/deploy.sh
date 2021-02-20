780a8f3dce98ee0fdf39326cd19ca148064905d9
Bryan Brady <bryan.brady@gmail.com>
2021-02-20 13:46:11 -0800

    update deploy script

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -11,21 +11,11 @@ git config --global user.email "bryan.brady@gmail.com"
 git config --global user.name "bryan brady"
 git clone git@github.com:bryanbrady/asdf-sh.git
 mkdir -p asdf-sh/wasm-game-of-life
-git status
 rm -rf asdf-sh/wasm-game-of-life/*
-git status
 cp dist/* asdf-sh/wasm-game-of-life/
-git status
 cd asdf-sh
-pwd
-ls
-ls wasm-game-of-life/
 if ! git diff --exit-code; then
-  git status
-  git add -u
-  git status
   git add .
-  git status
   git commit -m "https://github.com/bryanbrady/wasm-game-of-life/commit/$sha"
   git push origin master
 fi
